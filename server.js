0099422458f010e3285e7428c66a2e99ec448b9c
clark <clarklayos123@gmail.com>
2026-09-13 01:39:06 +0800

    initial changes

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello, World!");
 });
 
 app.listen(PORT, () => {
